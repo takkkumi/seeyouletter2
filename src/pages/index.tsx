d1d7651e080e2736b63f98d19eff68805764464c
takkkumi <nenetyankawaii@gmail.com>
2021-08-30 14:40:43 +0000

    init firebase

---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+import {Box,Text} from "@chakra-ui/react"
+function HomePage() {
+    return <Box padding={10} backgroundColor="blue.100">
+      <Text fontSize="xl" fontStyle="initial" position="static">Welcome to Next.js!</Text></Box>;
+  }
+  
+  export default HomePage;
